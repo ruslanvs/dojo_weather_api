c8780c7bd6f3337cae01ce7efaebc802199da4da
ruslanvs <ruslanvs@yahoo.com>
2018-02-18 20:22:18 -0800

    Default Angular Page

---
+++ server.js
@@ -0,0 +1,19 @@
+console.log( "******** server.js ********" );
+
+let express = require( "express" );
+let app = express();
+
+let bodyParser = require( "body-parser" );
+let path = require( "path" );
+let mongoose = require( "mongoose" );
+
+app.use( bodyParser.urlencoded( { extended: true } ) );
+app.use( bodyParser.json() );
+
+app.use( express.static( __dirname + "/client/dist" ) );
+
+mongoose.connect( "mongodb://localhost/dojo_weather_api" );
+
+app.listen( 8000, function(){
+    console.log( "listening on port 8000" );
+});
